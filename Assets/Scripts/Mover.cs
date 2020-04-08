9a95bc8729ca2a4e7ca42a486b29a4c44b6ed928
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-07 22:17:25 -0700

    Started on NavMesh following a target.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = _target.position;
+    }
+}
