994abef0d238dbd91eb4e89670a492171fba8df5
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-08 12:38:08 -0700

    Raycasting and drawing Debug.Ray

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,16 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
+    private Ray _lastRay;
+
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100f);
+
         GetComponent<NavMeshAgent>().destination = _target.position;
     }
 }
