cde90e437def231d37efdbc927a443550e6185e3
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-12 16:40:11 -0700

    Moving within range. Attack cancels movement, and movement cancels attack. Introduced interfaces to avoid circular dependencies.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,12 +1,43 @@
 using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] private float _weaponRange = 2f;
+
+        private Transform _target;
+
+        private void Update()
         {
-            print("Take that you short, squat peasant!");
+            if(_target == null) return;
+
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(_target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, _target.position) < _weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            _target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            _target = null;
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,27 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         [SerializeField] private Transform _target;
 
-        void Update()
+        private NavMeshAgent _navMeshAgent;
+
+        private void Start() 
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
         {
             UpdateAnimator();
         }
 
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;
+            _navMeshAgent.destination = destination;
+            _navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            _navMeshAgent.isStopped = true;
         }
 
         private void UpdateAnimator()
         {
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = _navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
