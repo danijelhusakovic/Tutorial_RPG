4542fc838241faee9f8faa23b063db8a79ca1f36
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-16 19:20:46 -0700

    Added the option to choose which portal location we're teleporting to. Useful if there are more portals in the same level.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,18 +1,64 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] private int _sceneToLoad = -1;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private DestinationIdentifier _destination;
 
         private void OnTriggerEnter(Collider other) 
         {
             if(!other.tag.Equals("Player")) { return; }
 
-            print("Portal triggered");
-            SceneManager.LoadScene(_sceneToLoad);
+            
+            StartCoroutine(Transition());
+        }
+
+        private IEnumerator Transition()
+        {
+            if(_sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load is not set");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(_sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) continue;
+                if(portal._destination != _destination) { continue; }
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal._spawnPoint.position);
+            player.transform.rotation = otherPortal._spawnPoint.rotation;
         }
     }
 }
