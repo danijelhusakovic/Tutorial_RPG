641b312d20cad6dfa40c0ebb6a81a2012d94fe39
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-19 12:41:03 -0700

    Started working on Stats / progression. Created a Scriptable Object with serializable lists of lists.

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] private int _startingLevel = 1;
+        [SerializeField] private CharacterClass _characterClass;
+        [SerializeField] private Progression _progression = null;
+    }
+}
---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] _characterClasses = null;
+
+        [System.Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] private CharacterClass _characterClass;
+            [SerializeField] private float[] _health;
+        }
+    }
+}
