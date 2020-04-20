55e250cd5973a1ad0d6391ce0abd78136721e41f
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-20 15:58:06 -0700

    Saving the experience points. Also displaying them in the HUD.

--- Assets/Scripts/Resources/Experience.cs
+++ Assets/Scripts/Resources/Experience.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] private float _experiencePoints = 0f;
 
         public void GainExperience(float experience)
         {
             _experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return _experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            _experiencePoints = (float) state;
+        }
+
+        public float GetPoints()
+        {
+            return _experiencePoints;
         }
     }
 }
---
+++ Assets/Scripts/Resources/ExperienceDisplay.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Resources
+{
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        private Experience _experience;
+
+        private void Awake() 
+        {
+            _experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+        }
+
+        private void Update() 
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", _experience.GetPoints());
+        }
+    }
+}
