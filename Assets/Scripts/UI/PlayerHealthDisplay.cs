2b5bada76ba27b1118d20a558898917d89b9ae2e
danijelhusakovic <danijel.husakovic@gmail.com>
2020-04-26 08:51:19 -0700

    Added Health UI display for the player.

---
+++ Assets/Scripts/UI/PlayerHealthDisplay.cs
@@ -0,0 +1,25 @@
+﻿using RPG.Attributes;
+using UnityEngine;
+
+namespace RPG.UI
+{
+
+    public class PlayerHealthDisplay : MonoBehaviour
+    {
+        [SerializeField] private Transform _foreground;
+
+        private Health _health;
+
+        private void Start()
+        {
+            _health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        }
+
+        private void Update()
+        {
+            float healthValue = Mathf.Clamp01(_health.GetFraction());
+            _foreground.localScale = new Vector3(healthValue, _foreground.localScale.y, _foreground.localScale.z);
+        }
+    }
+
+}
